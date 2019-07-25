4341383b39787c45ae23844a87d7a921e9e3051c
Allan de Miranda <allandemiranda@gmail.com>
2019-07-25 17:36:52 -0300

    config

--- segurancaOff.php
+++ segurancaOff.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "itep123";
+$username = "";
+$password = "";
 $dbname = "sistema_itep";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
