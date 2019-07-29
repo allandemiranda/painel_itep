8cb846c7031a3d21d3a66494bca04fc75bf9c694
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-07-29 08:42:21 -0300

    digitais ok

---
+++ necropapiloscopiaVerDigitais.php
@@ -0,0 +1,22 @@
+<?php
+include("seguranca.php"); // Inclui o arquivo com o sistema de segurança
+protegePagina(); // Chama a função que protege a página
+?>
+<?php
+if ($_GET["necropapiloscopia_digitais_folha_um"] != "") {
+    $PicNum = $_GET["necropapiloscopia_digitais_folha_um"];
+    $sql = "SELECT `necropapiloscopia_digitais_folha_um` FROM `necropapiloscopia_tb` WHERE `necropapiloscopia_protocolo`='" . $PicNum . "'";
+    $result = mysqli_query($_SG['link'], $sql) or die("Impossível executar a query ");
+    $row = mysqli_fetch_object($result);
+    Header("Content-type: image/gif");
+    echo $row->necropapiloscopia_digitais_folha_um;
+}
+if ($_GET["necropapiloscopia_digitais_folha_dois"] != "") {
+    $PicNum = $_GET["necropapiloscopia_digitais_folha_dois"];
+    $sql = "SELECT `necropapiloscopia_digitais_folha_dois` FROM `necropapiloscopia_tb` WHERE `necropapiloscopia_protocolo`='" . $PicNum . "'";
+    $result = mysqli_query($_SG['link'], $sql) or die("Impossível executar a query ");
+    $row = mysqli_fetch_object($result);
+    Header("Content-type: image/gif");
+    echo $row->necropapiloscopia_digitais_folha_dois;
+}
+?>
